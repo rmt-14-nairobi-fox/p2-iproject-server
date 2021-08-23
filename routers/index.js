9dcf2d8c2c70c54d8834d0973d5a6b3689259088
MarkTiger <marco.tiger.98@gmail.com>
2021-08-23 23:06:08 +0700

    Create routing and controller

--- routers/index.js
+++ routers/index.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const newsPrefsRouter = require('./newsprefs');
+const usersRouter = require('./users');
+const newsRouter = require('./news');
+const postsRouter = require('./posts');
+const commentsRouter = require('./comments');
+
+router.use('/newsprefs', newsPrefsRouter);
+
+router.use('/users', usersRouter);
+
+router.use('/news', newsRouter);
+
+router.use('/posts', postsRouter);
+
+router.use('/comments', commentsRouter);
 
 module.exports = router;
