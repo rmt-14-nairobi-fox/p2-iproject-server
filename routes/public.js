c21cc290150b3fb0fc88ba0981c39bd3e03bf811
asraavisena <asra.avisena@yahoo.com>
2021-08-24 00:23:47 +0700

    public register and login

--- routes/public.js
+++ routes/public.js
@@ -3,10 +3,22 @@ const { auth, authZCustomer } = require("../middlewares/auth");
 
 const SaveAccommodationController = require("../controllers/SaveAccommodationController");
 const AccommodationController = require("../controllers/AccommodationController");
+const UserController = require("../controllers/UserController");
+
+router.post("/register", UserController.registerPublic);
+
+router.post("/login", UserController.loginPublic);
 
 router.use(auth);
+// !SAVED
 router.get("/savedAccommodations", SaveAccommodationController.getAllSave);
 
+// ! USERS SIDE
+router.get("/profiles", UserController.userProfile);
+
+router.put("/edit", UserController.edit);
+
+// ! PUBLIC
 router.get("/", AccommodationController.getAllPublic);
 
 router.get("/:id", AccommodationController.getByIdPublic);
