1a408930c7d6abe76f01ab8bd8f41908ab36daac
gregorius-tech <gregoriusverly@gmail.com>
2021-08-24 16:35:53 +0700

    setup model

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const { User } = require("../models");
+class UserController {
+  static async register(req, res, next) {
+    try {
+    } catch (error) {}
+  }
+  static async login(req, res, next) {}
+  static async googleLogin(req, res, next) {}
+}
+
+module.exports = UserController;
