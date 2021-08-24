82d4d4331263de32106d8ec97c1912bc203ecb7e
KuraoHikari <dewaindra705@gmail.com>
2021-08-24 17:01:08 +0800

    login finish

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,5 @@
+const { checkPass } = require("../helper/bycript");
+const { jwtSign } = require("../helper/jwt");
 const { User } = require("../models");
 
 class UserController {
@@ -11,6 +13,24 @@ class UserController {
       };
       const result = await User.create(payload);
       res.status(201).json({ id: result.id, email: result.email });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  static async login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      const result = await User.findOne({ where: { email: email } });
+      if (result) {
+        if (checkPass(password, result.password)) {
+          const access_token = jwtSign({ id: result.id, email: result.email, username: result.username });
+          res.status(200).json({ access_token });
+        } else {
+          throw { name: "UnauthorizedLogin" };
+        }
+      } else {
+        throw { name: "UnauthorizedLogin" };
+      }
     } catch (err) {
       res.status(500).json(err);
     }
