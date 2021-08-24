718c7debc29e1c1d064d972136f1bb1bd585591a
gregorius-tech <gregoriusverly@gmail.com>
2021-08-25 03:00:53 +0700

    product seeder

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,20 +1,6 @@
 const { Service, Product, User } = require("../models");
 class CustController {
-  static async showProduct(req, res, next) {
-    const id = req.params.id;
-    try {
-      const result = await Product.findAll({
-        where: {
-          ServiceId: id,
-        },
-        include: [Service, User],
-      });
-      console.log(result);
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
-    }
-  }
+  static async showProduct(req, res, next) {}
   static async serviceAll(req, res, next) {
     try {
       const result = await Service.findAll();
