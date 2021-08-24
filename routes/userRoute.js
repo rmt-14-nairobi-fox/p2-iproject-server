73d48df91b3131b3e2b5ff43626df2d587b1b59b
gregorius-tech <gregoriusverly@gmail.com>
2021-08-24 18:16:16 +0700

    login clear

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const UserController = require("../controllers/userController");
 
 router.post("/register", UserController.register);
-// router.post("/login", UserController.login);
+router.post("/login", UserController.login);
 // router.post("/auth/google", UserController.googleLogin);
 
 module.exports = router;
