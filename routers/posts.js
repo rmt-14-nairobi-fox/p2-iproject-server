e7df62df43795353eb259ff73072a8a74487c254
MarkTiger <marco.tiger.98@gmail.com>
2021-08-24 12:42:07 +0700

    Move authentication to the top

--- routers/posts.js
+++ routers/posts.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const Controller = require('../controllers/posts');
 const authentication = require('../middlewares/authentication');
 
-router.get('/', Controller.getPosts);
-
 router.use(authentication);
+
+router.get('/', Controller.getPosts);
 
 router.post('/', Controller.createPost);
 
