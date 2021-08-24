dc099ff94109c54c61b72c1c54080e62a4ce9ba3
MarkTiger <marco.tiger.98@gmail.com>
2021-08-24 12:17:55 +0700

    posts endpoint, create new post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,24 @@
-class Controller {}
+const { Post } = require('../models');
+
+class Controller {
+  static async createPost(req, res, next) {
+    try {
+      const user_id = req.user.id;
+      const { title, message, savedNews_id } = req.body;
+      const payload = {
+        title,
+        message,
+        user_id,
+        savedNews_id,
+      };
+
+      await Post.create(payload);
+      res.status(201).json({ message: 'Post created' });
+    } catch (err) {
+      err.endpoint = req.baseUrl;
+      next(err);
+    }
+  }
+}
 
 module.exports = Controller;
--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const Controller = require('../controllers/posts');
 
+router.post('/', Controller.createPost);
+
 module.exports = router;
