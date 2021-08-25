85ba9f6c236c7a5fbb9a2b61e8d8ab39e5356300
asraavisena <asra.avisena@yahoo.com>
2021-08-26 04:26:17 +0700

    prep for deploy

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
@@ -5,7 +9,7 @@ const router = require("./routes");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
