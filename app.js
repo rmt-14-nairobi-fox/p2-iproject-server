5709653a0c11f0780207e15adb1a010ede23143f
asraavisena <asra.avisena@yahoo.com>
2021-08-26 04:56:50 +0700

    still trying to deploy

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
 
-require("dotenv").config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
