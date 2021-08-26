10b01102d22db984c8c461f1d159baa6679a40d7
Handson Panggabean <hansenpanggabean8@gmail.com>
2021-08-26 12:48:47 +0700

    deploying to heroku

--- app.js
+++ app.js
@@ -12,32 +12,32 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(router)
 
-const httpServer = require('http').createServer(app);
-const io = require("socket.io")(httpServer)
-const users = []
+// const httpServer = require('http').createServer(app);
+// const io = require("socket.io")(httpServer)
+// const users = []
 
-io.on('connection', (socket) => {
-  console.log('user connected');
+// io.on('connection', (socket) => {
+//   console.log('user connected');
 
-  socket.on('sendMessage', (data) => {
-    console.log(data, "ini di server");
+//   socket.on('sendMessage', (data) => {
+//     console.log(data, "ini di server");
 
-    // io.emit("broadcastMessage", data) // broadcast ke chatbox seharusnya
-    socket.broadcast.emit("broadcastMessage", data)
-  })
+//     // io.emit("broadcastMessage", data) // broadcast ke chatbox seharusnya
+//     socket.broadcast.emit("broadcastMessage", data)
+//   })
 
-  socket.on("userLogin", (user) => {
-    users.push(user);
-    // console.log(users);
-    io.emit("sendUser", users) //buat nampilin yang online di chatbox nya(jika sempat)
-  })
-})
-
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-})
-// app.listen(PORT, () => {
-//   console.log(`return SEMANGAT!!`);
+//   socket.on("userLogin", (user) => {
+//     users.push(user);
+//     // console.log(users);
+//     io.emit("sendUser", users) //buat nampilin yang online di chatbox nya(jika sempat)
+//   })
 // })
+
+// httpServer.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// })
+app.listen(PORT, () => {
+  console.log(`return SEMANGAT!!`);
+})
 
 module.exports = app
