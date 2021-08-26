69fef9680b9a5f63419135c8de3b31bcfb7368d5
Handson Panggabean <hansenpanggabean8@gmail.com>
2021-08-26 12:53:42 +0700

    deploy herokuu

--- app.js
+++ app.js
@@ -11,31 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(router)
-
-// const httpServer = require('http').createServer(app);
-// const io = require("socket.io")(httpServer)
-// const users = []
-
-// io.on('connection', (socket) => {
-//   console.log('user connected');
-
-//   socket.on('sendMessage', (data) => {
-//     console.log(data, "ini di server");
-
-//     // io.emit("broadcastMessage", data) // broadcast ke chatbox seharusnya
-//     socket.broadcast.emit("broadcastMessage", data)
-//   })
-
-//   socket.on("userLogin", (user) => {
-//     users.push(user);
-//     // console.log(users);
-//     io.emit("sendUser", users) //buat nampilin yang online di chatbox nya(jika sempat)
-//   })
-// })
-
-// httpServer.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// })
 app.listen(PORT, () => {
   console.log(`return SEMANGAT!!`);
 })
