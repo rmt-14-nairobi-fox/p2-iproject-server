3acda60e39da69982a12bb0b18053fe38eaff744
Alfian Rudi Yogatama <alvmarjinal@gmail.com>
2021-08-26 11:21:10 +0700

    deployheroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const router = require('./router')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
