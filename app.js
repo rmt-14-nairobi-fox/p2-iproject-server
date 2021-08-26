8df5086684f2a36ad0daa17a0641becf8f85482f
MarkTiger <marco.tiger.98@gmail.com>
2021-08-26 10:51:08 +0700

    Edit client url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: true,
-  origins: ['http://localhost:8080'],
+  origins: ['https://newromantimes.web.app'],
 });
 
 app.use(cors());
