e5e8be0e652932b1085e968612dfc9cdcff0aaa5
antirusuh <rizal.sufacete@gmail.com>
2021-08-27 02:02:25 +0700

    bikin public endpiont

--- controllers/public.js
+++ controllers/public.js
@@ -1,7 +1,19 @@
 const { User, Petition, Sign } = require('../models');
 
 class PublicController {
+    static async getAllPet(req, res, next) {
+        try {
+            const listP = await Petition.findAll({
+                where: {
+                    status: 'published'
+                }
+            })
 
+            res.status(200).json(listP)
+        } catch (err) {
+            next(err)
+        }
+    }
 }
 
 module.exports = PublicController
